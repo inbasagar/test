5eef808f8e2286ee43dcea534e5abd92f2f5bfc3
hrithik-r <hrithikmike10@gmail.com>
2024-01-03 22:29:54 +0530

    testing commit -hrithik

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/api/users",userRouter);
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 app.get('/', (req, res) => {
-  res.send('Hello test server!');
+  res.send('Hello test server from local!');
 });
 /* app.get("/api/products",(req,res)=>{
     res.json(products);
